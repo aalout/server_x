ec7fa0eff48cd876a0b6dce111d1932d023b8d3e
aalout <ilyan.wolf@gmail.com>
2024-02-01 12:49:19 +0300

    nest test p1

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,8 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const port = parseInt(process.env.PORT);
+  const server = process.env.SERVER;
+  await app.listen(port, server);
 }
 bootstrap();
