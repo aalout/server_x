460838428ea391a2f52bcc14436555650f4c051f
aalout <ilyan.wolf@gmail.com>
2024-02-06 10:35:32 +0300

    nest fix

--- src/main.ts
+++ src/main.ts
@@ -3,8 +3,6 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  const port = parseInt(process.env.PORT);
-  const server = process.env.SERVER;
-  await app.listen(port, server);
+  await app.listen(3000);
 }
 bootstrap();
